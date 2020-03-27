1e429fadc9b01c3d18baf2c96ca557fae3a1b9c9
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-27 17:03:12 +0100

    Inizializzazione dell'HTML | CSS | JS

---
+++ main.js
@@ -0,0 +1,6 @@
+// input nome utente
+var nomeUtente = document.getElementById('nome')
+// input km utente
+var kmUtente = document.getElementById('km')
+// input eta utente
+var etaUtente = document.getElementById('eta')
